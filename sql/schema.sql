f7dd26056a519ed1b5c3eb9ff96fad5e3d1293a9
Rafael Rocha <rafaelrocha241003@gmail.com>
2026-07-07 15:59:20 -0300

    Add views

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,47 +1,48 @@
--- Estrutura do banco de dados extraído da planilha --
+-- ESTRUTURA DO BANCO DE DADOS EXTRAÍDO DA PLANILHA --
 
 CREATE TABLE IF NOT EXISTS lancamentos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
 
-    conta TEXT CHECK (conta IN 'CIMMVI - Rateio Banco do Brasil', 'CIMMVI - Licenciamento Caixa', 'AMVI - Banco do Brasil - CC 439'),
+    conta TEXT CHECK (conta IN ('CIMMVI - Rateio Banco do Brasil', 'CIMMVI - Licenciamento Caixa', 'AMVI - Banco do Brasil - CC 439')),
 
-    entidade TEXT CHECK (entidade IN 'CIMMVI', 'AMVI'),
+    entidade TEXT CHECK (entidade IN ('CIMMVI', 'AMVI')),
 
-    banco TEXT, -- Banco do Brasil, Caixa, etc
+    banco TEXT, 
 
-    linha_planilha INTEGER NOT NULL, -- nº da linha da planilha original
+    linha_planilha INTEGER NOT NULL, 
 
     descricao TEXT,
 
-    nf_doc Varchar(30),
+    nf_doc VARCHAR(30),
 
     data_pagamento DATE,
 
-    situacao TEXT CHECK (situacao IN 'Pago', 'Em aberto', 'Aprovado - Aguardando Pagamento','Aguardando Aprovação', 'Pagamento Realizado - Aguardando autorização Margarete'),
+    situacao TEXT CHECK (situacao IN ('Pago', 'Em aberto', 'Aprovado - Aguardando Pagamento', 'Aguardando Aprovação', 'Pagamento Realizado - Aguardando autorização Margarete')),
 
-    forma_pagamento TEXT CHECK (forma_pagamento IN 'Pix', 'Boleto', 'DIRF'),
+    forma_pagamento TEXT CHECK (forma_pagamento IN ('Pix', 'Boleto', 'DIRF')),
 
     entradas NUMERIC(12,2) DEFAULT 0,
 
     saidas NUMERIC(12,2) DEFAULT 0,
 
-    valor_liquido NUMERIC(12,2) DEFAULT 0, -- entrada menos saída de cada linha
+    valor_liquido NUMERIC(12,2) DEFAULT 0, 
 
-    saldo_acumulado NUMERIC(12,2), -- Saldo acumulado do dia
+    saldo_acumulado NUMERIC(12,2), 
 
     observacao TEXT,
 
     tipo_lancamento TEXT NOT NULL DEFAULT 'MOVIMENTAÇÃO',
 
-    hash_linha TEXT NOT NULL, -- evita dados duplicados, é tratado nos arquivos .py
+    hash_linha TEXT NOT NULL, 
     UNIQUE(hash_linha)
 );
 
--- Views para o Dashboard
+
+-- VIEWS PARA O DASHBOARD --
 
 
 -- Saldo final de todas as 3 contas juntas
-DROP VIEW IF EXISTS vw_saldo_final_geral
+DROP VIEW IF EXISTS vw_saldo_final_geral;
 CREATE VIEW vw_saldo_final_geral AS
     WITH ultimo_saldo AS (
         SELECT
@@ -50,7 +51,7 @@ CREATE VIEW vw_saldo_final_geral AS
             ROW_NUMBER() OVER(
                 PARTITION BY conta
                 ORDER BY data_pagamento DESC, id DESC
-            ) AS id_ultimo_saldo -- pega a o útlimo saldo acumulado de cada dia e coloca de primeiro (índice 1) pra ser usado no where abaixo
+            ) AS id_ultimo_saldo -- pega a o útlimo saldo acumulado de cada conta e coloca de primeiro (índice 1) pra ser usado no where abaixo
         FROM lancamentos
     )
 SELECT 
@@ -60,14 +61,33 @@ SELECT
 
 
 -- Saldo final da conta 1 (CIMMVI - Rateio Banco do Brasil)
-
-
-
+DROP VIEW IF EXISTS vw_saldo_final_conta_1;
+CREATE VIEW vw_saldo_final_conta_1 AS
+SELECT
+    saldo_acumulado AS saldo_final
+FROM lancamentos
+WHERE conta = 'CIMMVI - Rateio Banco do Brasil'
+ORDER BY data_pagamento DESC, id DESC
+LIMIT 1;
 
 
 -- Saldo final da conta 2 (CIMMVI - Licenciamento Caixa)
-
-
+DROP VIEW IF EXISTS vw_saldo_final_conta_2;
+CREATE VIEW vw_saldo_final_conta_2 AS
+SELECT 
+    saldo_acumulado AS saldo_final
+FROM lancamentos
+WHERE conta = 'CIMMVI - Licenciamento Caixa'
+ORDER BY data_pagamento DESC, id DESC
+LIMIT 1;
 
 
 -- Saldo final da conta 3 (AMVI - Banco do Brasil - CC 439)
+DROP VIEW IF EXISTS vw_saldo_final_conta_3
+CREATE VIEW vw_saldo_final_conta_3 AS
+SELECT
+    saldo_acumulado AS saldo_final
+FROM lancamentos
+WHERE conta = 'AMVI - Banco do Brasil - CC 439'
+ORDER BY data_pagamento DESC, id DESC
+LIMIT 1;
